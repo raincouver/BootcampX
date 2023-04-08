62b23e0af2034908fe81fc049f4e3b9b766e13f3
Rain Zhu <rain1995zhu@gmail.com>
2023-04-08 06:24:40 -0700

    add parameterized queries

--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -23,17 +23,27 @@ const pool = new Pool({
 // }).catch(err => console.error('query error', err.stack));
 
 
-pool.query(`
+
+
+
+const cohortName = process.argv[2];
+
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+}).catch(err => console.error('query error', err.stack));
